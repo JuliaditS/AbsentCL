96ff606a9e5c6eaa8f70a3acf79dcc6a771fee4c
JuliaditS <36404399+JuliaditS@users.noreply.github.com>
2022-02-14 21:58:32 +0700

    Add files via upload

---
+++ Absensi.py
@@ -0,0 +1,43 @@
+from time import sleep
+from telethon.sync import TelegramClient, events
+from telethon.tl.types import *
+from telethon.tl.functions.messages import *
+from datetime import datetime
+
+api_id = 9163961
+api_hash = '66d6abc6922f89482382484b7b8b717a'
+# Login Client
+client = TelegramClient('anon', api_id, api_hash)
+
+Description = 'Codelabs Health Attendance'
+Option = [PollAnswer('Healthy', b'1'),PollAnswer('Mildly ill', b'2'),PollAnswer('Seriously ill', b'3')]
+
+@client.on(events.NewMessage)
+async def my_event_handler(event):
+
+# CodeLabs has ID -1001381374413
+
+# Kementerian Riset dan Teknologi CL 2022/2023 has ID -789267139
+
+    if '/start' in event.raw_text:
+        loop = True
+        while loop:
+            time = datetime.now().strftime("%H %M %S")
+            sleep(1)
+            if time == '21 46 00':
+                await client.send_message(-686884071,file=InputMediaPoll(
+                    poll=Poll(
+                        id=45,
+                        question=Description,
+                        answers=Option,
+                        public_voters=True
+                    )
+            )) 
+            elif '/stop' in event.raw_text:
+                loop = False
+    elif '/getidchat' in event.raw_text:
+        chatid=event.chat_id
+        await client.send_message(chatid, str(chatid))
+
+client.start()
+client.run_until_disconnected()
